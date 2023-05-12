68d4fa2c463b32c40add62e8db7bbafc0af3dbbf
Segoo <68073260+Michikatsu0@users.noreply.github.com>
2023-05-11 21:48:09 -0500

    Oscilation Done

--- Assets/Oscillation/Scripts/Exercises/Step3_1.cs
+++ Assets/Oscillation/Scripts/Exercises/Step3_1.cs
@@ -1,58 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.FilePathAttribute;
 
 public class Step3_1 : MonoBehaviour
 {
-    List<Oscillator_Step3_1> oscilattors = new List<Oscillator_Step3_1>();
-    List<Mover2_Step2_2> movers = new List<Mover2_Step2_2>();
-
-    Attractor2_Step2_2 attractor;
+    // Create a list to store multiple Movers
+    [SerializeField] float delay;
+    List<Mover3_Step3_1> movers = new List<Mover3_Step3_1>();
+    List<Attractor2_Step3_2> l_attractors = new List<Attractor2_Step3_2>();
+    float time = 0;
+    int attractorIndex = 0;
+    // Start is called before the first frame update
     void Start()
     {
-        int numberOfMovers = 13;
+        int numberOfMovers = 40;
         for (int i = 0; i < numberOfMovers; i++)
         {
             Vector2 randomLocation = new Vector2(Random.Range(-7f, 7f), Random.Range(-7f, 7f));
             Vector2 randomVelocity = new Vector2(Random.Range(0f, 5f), Random.Range(0f, 5f));
-            Mover2_Step2_2 m = new Mover2_Step2_2(Random.Range(0.4f, 1.4f), randomVelocity, randomLocation); //Each Mover is initialized randomly.
+            Mover3_Step3_1 m = new Mover3_Step3_1(Random.Range(0.2f, 1f), randomVelocity, randomLocation); //Each Mover is initialized randomly.
             movers.Add(m);
         }
-        attractor = new Attractor2_Step2_2();
-        while (oscilattors.Count < 10)
-        {
-            oscilattors.Add(new Oscillator_Step3_1());
 
+        int nomberOfAtractors = 1;
+        for (int i = 0; i < nomberOfAtractors; i++)
+        {
+            Attractor2_Step3_2 a = new Attractor2_Step3_2();
+            l_attractors.Add(a);
+        }
+        foreach (Attractor2_Step3_2 attractor in l_attractors)
+        {
+            attractor.attractor.SetActive(false);
         }
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        if (time >= delay)
+        {
+            attractorIndex++;
+            time = 0;
+        }
+        time += Time.deltaTime;
+
+        foreach (Attractor2_Step3_2 attractor in l_attractors)
+        {
+            attractor.attractor.SetActive(false);
+        }
+
+        if (attractorIndex == l_attractors.Count)
+            attractorIndex = 0;
+
+        l_attractors[attractorIndex].attractor.SetActive(true);
+
+        float x =Mathf.Cos(l_attractors[attractorIndex].angle.x) * l_attractors[attractorIndex].amplitude.x;
+
+        //Each oscillator object oscillating on the y-axis
+        float y = Mathf.Cos(l_attractors[attractorIndex].angle.y) * l_attractors[attractorIndex].amplitude.y;
+
+        l_attractors[attractorIndex].angle += l_attractors[attractorIndex].body.velocity;
+
+        l_attractors[attractorIndex].attractor.transform.Translate(new Vector2(x, y) * Time.deltaTime);
+
+
+        l_attractors[attractorIndex].CheckEdges();
+    }
+
     void FixedUpdate()
     {
-        foreach (Oscillator_Step3_1 o in oscilattors)
-        {
-            //Each oscillator object oscillating on the x-axis
-            float x = Mathf.Cos(o.angle.x) * o.amplitude.x;
-
-            //Each oscillator object oscillating on the y-axis
-            float y = Mathf.Cos(o.angle.y) * o.amplitude.y;
-
-            //Add the oscillator's velocity to its angle
-            o.angle += o.velocity;
-
-            // Draw the line for each oscillator
-            o.lineRender.SetPosition(1, o.oGameObject.transform.position);
-
-            //Move the oscillator
-            o.oGameObject.transform.transform.Translate(new Vector2(x, y) * Time.deltaTime);
-        }
-        foreach (Mover2_Step2_2 m in movers)
+        foreach (Mover3_Step3_1 m in movers)
         {
             Rigidbody body = m.body;
-            Vector2 attractForce = attractor.Attract(body); // Apply the attraction from the Attractor on each Mover object
-            Vector2 repelForce = attractor.Repel(body);
-            m.ApplyForce(attractForce * 10);
-            m.ApplyForce(repelForce * 0.08f);
+            Vector2 force = l_attractors[attractorIndex].Attract(body); // Apply the attraction from the Attractor on each Mover object
+
+            m.ApplyForce(force);
+
+            //Each oscillator object oscillating on the x-axis
+            float x = Random.Range(-10f, 10f) + Mathf.Cos(m.angle.x) * m.amplitude.x;
+
+            //Each oscillator object oscillating on the y-axis
+            float y = Random.Range(-5f, 5f) + Mathf.Cos(m.angle.y) * m.amplitude.y;
+
+            //Add the oscillator's velocity to its angle
+            m.angle += m.body.velocity;
+
+            // Draw the line for each oscillator
+            m.lineRender.SetPosition(0, l_attractors[attractorIndex].attractor.transform.position);
+            m.lineRender.SetPosition(1, m.body.transform.position);
+
+            //Move the oscillator
+            m.body.transform.transform.Translate(new Vector2(x, y) * Time.deltaTime);
+
             m.CalculatePosition();
         }
 
@@ -64,10 +103,10 @@ public class Step3_1 : MonoBehaviour
                 {
                     // Now that we are sure that our Mover will not attract itself, we need it to attract a different Mover
                     // We do that by directing a mover to use their Attract() method on another mover Rigidbodys
-                    Vector2 attractedMover = movers[j].Attract(movers[i].body);
+                    Vector2 attractedMover = movers[j].Repel(movers[i].body);
 
                     // We then apply that force the mover with the Rigidbody's Addforce() method
-                    movers[i].body.AddForce(attractedMover, ForceMode.Impulse);
+                    movers[i].body.AddForce(attractedMover * 0.08f, ForceMode.Impulse);
                 }
             }
             // Now we check the boundaries of our scene to make sure the movers don't fly off
@@ -78,35 +117,130 @@ public class Step3_1 : MonoBehaviour
     }
 }
 
-public class Oscillator_Step3_1
+
+public class Attractor2_Step3_2
 {
+    public Vector3 angle, amplitude;
+    private float radius;
+    private float mass;
+    private float G;
 
-    // The basic properties of an oscillator class
-    public Vector2 velocity, angle, amplitude, center;
+    private Vector2 location;
 
-    // The window limits
-    private Vector2 maximumPos;
+    public Rigidbody body;
+    public GameObject attractor;
 
-    // Gives the class a GameObject to draw on the screen
-    public GameObject oGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    public Vector2 maximumPos;
 
-    // Create variables for rendering the line between two vectors
-    public LineRenderer lineRender;
-
-    public Oscillator_Step3_1()
+    public Attractor2_Step3_2()
     {
         FindWindowLimits();
 
-        // Initialize the angle at 0, 0
-        angle = Vector2.zero;
+        attractor = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Object.Destroy(attractor.GetComponent<SphereCollider>());
+        Renderer renderer = attractor.GetComponent<Renderer>();
 
-        // Generate a random velocity and amplitude for each oscillator
-        velocity = new Vector2(Random.Range(-.05f, .05f), Random.Range(-0.05f, 0.05f));
+        body = attractor.AddComponent<Rigidbody>();
         amplitude = new Vector2(Random.Range(-maximumPos.x / 2, maximumPos.x / 2), Random.Range(-maximumPos.y / 2, maximumPos.y / 2));
+        body.position = new Vector3(Random.Range(-20f, 20f), Random.Range(-10f, 10f), 0f);
+        body.velocity = new Vector2(Random.Range(-.05f, .05f), Random.Range(-0.05f, 0.05f));
+        // Generate a radius
+        radius = 4;
 
-        // Create a new material for WebGL
-        Renderer r = oGameObject.GetComponent<Renderer>();
-        r.material = new Material(Shader.Find("Diffuse"));
+        // Place our mover at the specified spawn position relative
+        // to the bottom of the sphere
+        attractor.transform.position = body.position;
+
+        // The default diameter of the sphere is one unit
+        // This means we have to multiple the radius by two when scaling it up
+        attractor.transform.localScale = radius * Vector3.one;
+
+        // We need to calculate the mass of the sphere.
+        // Assuming the sphere is of even density throughout,
+        // the mass will be proportional to the volume.
+        body.mass = (4f / 3f) * Mathf.PI * radius * radius * radius;
+        body.useGravity = false;
+        body.isKinematic = true;
+
+        renderer.material = new Material(Shader.Find("Diffuse"));
+        renderer.material.color = Color.red;
+        renderer.enabled = false;
+        G = 9.8f;
+    }
+
+    public Vector2 Attract(Rigidbody m)
+    {
+        Vector2 force = body.position - m.position;
+        float distance = force.magnitude;
+
+        // Remember we need to constrain the distance so that our circle doesn't spin out of control
+        distance = Mathf.Clamp(distance, 5f, 25f);
+
+        force.Normalize();
+        float strength = (G * body.mass * m.mass) / (distance * distance);
+        force *= strength;
+        return force;
+    }
+
+
+    public void CheckEdges()
+    {
+        location = attractor.transform.position;
+
+        if (location.x > maximumPos.x)
+        {
+            location.x = -maximumPos.x;
+        }
+        else if (location.x < -maximumPos.x)
+        {
+            location.x = maximumPos.x;
+        }
+        if (location.y > maximumPos.y)
+        {
+            location.y = -maximumPos.y;
+        }
+        else if (location.y < -maximumPos.y)
+        {
+            location.y = maximumPos.y;
+        }
+
+        attractor.transform.position = location;
+    }
+
+
+    private void FindWindowLimits()
+    {
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 15;
+        Camera.main.transform.position = new Vector3(0, 0, -10);
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+}
+
+public class Mover3_Step3_1
+{
+    public Vector3 angle, amplitude, center;
+    // The basic properties of a mover class
+    public Rigidbody body;
+    private Transform transform;
+    private GameObject mover;
+
+    private Vector2 maximumPos;
+    public LineRenderer lineRender;
+
+    public Mover3_Step3_1(float randomMass, Vector2 initialVelocity, Vector2 initialPosition)
+    {
+        mover = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Object.Destroy(mover.GetComponent<SphereCollider>());
+        transform = mover.transform;
+
+        mover.AddComponent<Rigidbody>();
+        body = mover.GetComponent<Rigidbody>();
+        body.useGravity = false;
+
+        Renderer renderer = mover.GetComponent<Renderer>();
+        renderer.material = new Material(Shader.Find("Diffuse"));
+        mover.transform.localScale = new Vector3(randomMass, randomMass, randomMass);
 
         // Create a GameObject that will be the line
         GameObject lineDrawing = new GameObject();
@@ -114,19 +248,60 @@ public class Oscillator_Step3_1
         // Add the Unity Component "LineRenderer" to the GameObject lineDrawing.
         lineRender = lineDrawing.AddComponent<LineRenderer>();
         lineRender.material = new Material(Shader.Find("Diffuse"));
+        amplitude = new Vector2(Random.Range(-maximumPos.x / 2, maximumPos.x / 2), Random.Range(-maximumPos.y / 2, maximumPos.y / 2));
 
-        // Begin rendering the line between the two objects. Set the first point (0) at the centerSphere Position
-        // Make sure the end of the line (1) appears at the new Vector3 in FixedUpdate()
-        lineRender.SetPosition(0, center);
+
+        body.mass = randomMass;
+        body.position = initialPosition; // Default location
+        body.velocity = initialVelocity; // The extra velocity makes the mover orbit
+        FindWindowLimits();
+    }
+
+    public void ApplyForce(Vector2 force)
+    {
+        body.AddForce(force, ForceMode.Force);
+    }
+
+    public void CalculatePosition()
+    {
+        CheckEdges();
+    }
+
+    private void CheckEdges()
+    {
+        Vector2 velocity = body.velocity;
+        if (transform.position.x > maximumPos.x || transform.position.x < -maximumPos.x)
+        {
+            velocity.x *= -1 * Time.deltaTime;
+        }
+        if (transform.position.y > maximumPos.y || transform.position.y < -maximumPos.y)
+        {
+            velocity.y *= -1 * Time.deltaTime;
+        }
+        body.velocity = velocity;
+    }
+
+    public Vector2 Repel(Rigidbody m)
+    {
+        Vector2 force = body.position - m.position;
+        float distance = force.magnitude;
+
+        // Remember we need to constrain the distance so that our circle doesn't spin out of control
+        distance = Mathf.Clamp(distance, 5f, 25f);
+
+        force.Normalize();
+        float strength = (9.81f * body.mass * m.mass) / (distance * distance);
+        force *= -strength;
+        return force;
     }
 
     private void FindWindowLimits()
     {
-        // We want to start by setting the camera's projection to Orthographic mode
         Camera.main.orthographic = true;
-        // For correct functionality, we set the camera x and y position to 0, 0
+        Camera.main.orthographicSize = 15;
         Camera.main.transform.position = new Vector3(0, 0, -10);
-        // Next we grab the minimum and maximum position for the screen
         maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 }
+
+
